d0838520612aa0ccfc979656e75a88f28e39201e
Jacob Grimm <jacobgrimm@utexas.edu>
2020-03-30 00:56:04 -0500

    tried fixing error

--- putInDatabase.py
+++ putInDatabase.py
@@ -4,35 +4,6 @@ import json
 
 import sqlalchemy
 
-directory = 'Issues/'
-for jsonFile  in  os.listdir(directory):
-    importantFile = open(directory + jsonFile)
-    issueDict = json.load(importantFile)
-    newList = []
-    for i in issueDict['character_credits']:
-        newList.append(i.replace("'",""))
-    issueDict['character_credits'] = newList
-    newList2 = []
-    for i in issueDict['person_credits']:
-        newList2.append(i.replace("'",""))
-    issueDict['person_credits'] = newList2
-
-    issueDict['name'] = issueDict['name'].replace("'","")
-    issueDict['series'] = issueDict['series'].replace("'","")
-    issueDict['cover_date'] = issueDict['cover_date'].replace("'","")
-    issueDict['description'] = issueDict['description'].replace("'","")
-
-
-    with open(directory+jsonFile, 'w+') as out_file:
-        out_file.write(json.dumps(issueDict, sort_keys=True, indent=4 * ' '))
-
-
-print("INSERT INTO Issues(Title, Series, ReleaseDate, Description, ImageURL, Authors, Characters) values('{}','{}','{}','{}','{}','{{ {} }}','{{ {} }}');".format(
-                    issueDict['name'], issueDict['series'],issueDict['cover_date'],issueDict['description'],issueDict['image'],json.dumps(issueDict['person_credits']),
-                    json.dumps(issueDict['character_credits'])))
-
-
-
 
 def start():
     cloud_sql_connection_name = 'icdb-sql:us-central1:mysql-test'
@@ -61,8 +32,7 @@ def start():
             for jsonFile  in  os.listdir(directory):
                 importantFile = open(directory + jsonFile)
                 issueDict = json.load(importantFile)
-                conn.execute("INSERT INTO Issues(Title, Series, ReleaseDate, Description, ImageURL, Authors, Characters) values('{}','{}','{}','{}','{}','{{ {} }}','{{ {} }}');".format(
+                query = "INSERT INTO Issues(Title, Series, ReleaseDate, Description, ImageURL, Authors, Characters) values('{}','{}','{}','{}','{}','{{ {} }}','{{ {} }}');".format(
                     issueDict['name'], issueDict['series'],issueDict['cover_date'],issueDict['description'],issueDict['image'],json.dumps(issueDict['person_credits']),
                     json.dumps(issueDict['character_credits']))
-                
-    )
+                conn.execute(query)
