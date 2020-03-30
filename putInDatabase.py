97634c416ae5897f6501f0f12f11562ca0c56cca
Minalinnski <36867760+Minalinnski@users.noreply.github.com>
2020-03-30 12:38:39 -0500

    update some code for char and author

--- putInDatabase.py
+++ putInDatabase.py
@@ -36,3 +36,22 @@ def start():
                     issueDict['name'], issueDict['series'],issueDict['cover_date'],issueDict['description'],issueDict['image'],'"person_credits": ' + json.dumps(issueDict['person_credits']),
                     '"character_credits": ' +json.dumps(issueDict['character_credits']))
                 conn.execute(query)
+                
+    for directory in directories:
+        with db.connect() as conn:
+            for jsonFile  in  os.listdir(directory):
+                importantFile = open(directory + jsonFile)
+                charDict = json.load(importantFile)
+                query = "INSERT INTO Characters(HeroName, RealName, Aliases, Alignment, Appearance, Creators, Deck, Description, FirstAppearance, ImageURL) values('{}','{}','{}','{}','{{ {} }}','{{ {} }}','{}','{}','{}','{}');".format(
+                    charDict['name'],charDict['real_name'],charDict['aliases'],charDict['alignment'],'"appearance:":' + json.dumps(charDict['appearance']),'"creators":'+ json.dumps(charDict['creators']),
+                    charDict['deck'],charDict['discription'],charDict['first_appeared_in_issue'],charDict['image']))
+                conn.execute(query)
+                
+     for directory in directories:
+        with db.connect() as conn:
+            for jsonFile  in  os.listdir(directory):
+                importantFile = open(directory + jsonFile)
+                authorDict = json.load(importantFile)
+                query = "INSERT INTO Authors(Name, Aliases, Birth, Country, Death, Deck, Description, Hometown, ImageURL) values('{}','{}','{}','{}','{}','{}','{}','{}','{}');".format(
+                    authorDict['name'],authorDict['aliases'],authorDict['birth'],authorDict['country'],authorDict['death'],authorDict['deck'],authorDict['description'],authorDict['hometown'],authorDict['image']))
+                conn.execute(query)
