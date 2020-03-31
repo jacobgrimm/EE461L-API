9274d81666730b5e9ef965d4063e86e8e339a14f
Minalinnski <36867760+Minalinnski@users.noreply.github.com>
2020-03-30 20:32:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ def test_access_success():
     response = requests.get("http://super-phase2-api.appspot.com/")
     assert response.status_code == 200
     
+#test Characters
 def test_char_access():
     response = requests.get("http://super-phase2-api.appspot.com/characters")
     response_body = response.json()
@@ -37,16 +38,107 @@ def test_char_byName():
     assert response_body['results']['real_name']=="Peter Benjamin Parker"
     
     
+#test issues    
+def test_issue_access():
+    response = requests.get("http://super-phase2-api.appspot.com/issues")
+    response_body = response.json()
+    assert response_body["response"]=="Success"
+    
+def test_issue_page():
+    response = requests.get("http://super-phase2-api.appspot.com/issues")
+    response_body = response.json()
+    assert response_body['page_num']==1
+    
+def test_issue_page_count():
+    response = requests.get("http://super-phase2-api.appspot.com/issues")
+    response_body = response.json()
+    assert response_body['pages_total']==5
+    
+def test_issue_detail():
+    response = requests.get("http://super-phase2-api.appspot.com/issues")
+    response_body = response.json()
+    assert response_body['results'][0]['cover_date'] == "1991-10-01"
+    
+def test_issue_page():
+    response = requests.get("http://super-phase2-api.appspot.com/issues/2")
+    response_body = response.json()
+    assert response_body['page_num']==2
+    
+def test_issue_page_content():
+    response = requests.get("http://super-phase2-api.appspot.com/issues/2")
+    response_body = response.json()
+    assert response_body['results'][0]['name']=="Introducing the Sensational Black Panther"
+    
+def test_issue_byName():
+    response = requests.get("http://super-phase2-api.appspot.com/issue/Made In China Part One")
+    response_body = response.json()
+    assert response_body['results']['series']=="New Super-Man"
+    
+#test authors
+def test_author_access():
+    response = requests.get("http://super-phase2-api.appspot.com/authors")
+    response_body = response.json()
+    assert response_body["response"]=="Success"
+    
+def test_author_page():
+    response = requests.get("http://super-phase2-api.appspot.com/authors")
+    response_body = response.json()
+    assert response_body['page_num']==1
+    
+def test_author_page_count():
+    response = requests.get("http://super-phase2-api.appspot.com/authors")
+    response_body = response.json()
+    assert response_body['pages_total']==7
+    
+def test_author_detail():
+    response = requests.get("http://super-phase2-api.appspot.com/authors")
+    response_body = response.json()
+    assert response_body['results'][4]['name'] == "Billy Tan"
+    
+def test_author_page():
+    response = requests.get("http://super-phase2-api.appspot.com/authors/3")
+    response_body = response.json()
+    assert response_body['page_num']==3
+    
+def test_author_content():
+    response = requests.get("http://super-phase2-api.appspot.com/authors/2")
+    response_body = response.json()
+    assert response_body['results'][0]['name']=="Introducing the Sensational Black Panther"
+    
+def test_author_byName():
+    response = requests.get("http://super-phase2-api.appspot.com/author/Stan Lee")
+    response_body = response.json()
+    assert response_body['results']['hometown']=="New York City"
+    
+    
     
 def main():
     test_access_success()
+    
     test_char_access()
     test_char_page()
     test_char_page_count()
     test_char_detail()
     test_char_page()
     test_char_byName()
-    print("pass!")
+    
+    test_issue_access()
+    test_issue_page()
+    test_issue_page_count()
+    test_issue_detail()
+    test_issue_page()
+    test_issue_page_content()
+    test_issue_byName()
+    
+    test_author_access()
+    test_author_page()
+    test_author_page_count()
+    test_author_detail()
+    test_author_page()
+    test_author_page_content()
+    test_author_byName()
+    
+    print("pass all!")
     
     
     
