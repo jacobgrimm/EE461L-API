c8b1a3abcd2f062488aac12c30b1fce11b7884f4
Jacob Grimm <jacobgrimm@utexas.edu>
2020-05-01 20:33:42 -0500

    extrcted getSearchResults method

--- Search.py
+++ Search.py
@@ -10,32 +10,8 @@ def searchFor(db,term, pageNum):
 
     conn = db.connect()
 
-    issueResultproxy = conn.execute("""SELECT Title, MATCH(Title, Series) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Issues WHERE MATCH(Title, Series) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
-    issueResults = sqlToDict(issueResultproxy)
-    for issueDict in issueResults:
-        issueDict['type'] = 'issue'
-        issueDict['name'] = issueDict['Title'].replace('\"','"')
-        del(issueDict['Title'])
-
-        
-    charResultproxy = conn.execute("""SELECT HeroName, MATCH(HeroName, RealName, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Characters WHERE MATCH(HeroName, RealName, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
-    charResults = sqlToDict(charResultproxy)
-    for charDict in charResults:
-        charDict['type'] = 'character'
-        charDict['name']= charDict['HeroName']
-        del charDict['HeroName']
-
-
-    authorResultproxy = conn.execute("""SELECT Name, MATCH(Name, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Authors WHERE MATCH(Name, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
-    authorResults = sqlToDict(authorResultproxy)
-    for authorDict in authorResults:
-        authorDict['type'] = 'author'
-        authorDict['name']= authorDict['Name']
-        del authorDict['Name']
-
+    results = getSearchResults(conn, term)
     
-    results = issueResults + charResults + authorResults
-
     if len(results) == 0 :
         resp['response'] = "term '{}' not found in database".format(term)
         resp['result'] = 'null'
@@ -65,4 +41,34 @@ def searchFor(db,term, pageNum):
     resp.headers['Access-Control-Allow-Origin'] = '*'
 
     return resp
+
+def getSearchResults(conn, term):
+    issueResultproxy = conn.execute("""SELECT Title, MATCH(Title, Series) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Issues WHERE MATCH(Title, Series) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
+    issueResults = sqlToDict(issueResultproxy)
+    for issueDict in issueResults:
+        issueDict['type'] = 'issue'
+        issueDict['name'] = issueDict['Title'].replace('\"','"')
+        del(issueDict['Title'])
+
+        
+    charResultproxy = conn.execute("""SELECT HeroName, MATCH(HeroName, RealName, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Characters WHERE MATCH(HeroName, RealName, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
+    charResults = sqlToDict(charResultproxy)
+    for charDict in charResults:
+        charDict['type'] = 'character'
+        charDict['name']= charDict['HeroName']
+        del charDict['HeroName']
+
+
+    authorResultproxy = conn.execute("""SELECT Name, MATCH(Name, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE) AS score FROM Authors WHERE MATCH(Name, Aliases) AGAINST('{}' IN NATURAL LANGUAGE MODE);""".format(term,term))
+    authorResults = sqlToDict(authorResultproxy)
+    for authorDict in authorResults:
+        authorDict['type'] = 'author'
+        authorDict['name']= authorDict['Name']
+        del authorDict['Name']
+
+    
+    results = issueResults + charResults + authorResults
+    return results
+
+
 
